451ffabefb6ee8c49c62b859bc2e58f2b331cd3f
Damon Estrada <damon.estrada@student.nmt.edu>
2021-06-22 21:00:49 -0700

    Here is my slow java implementation! Without the outer class. Saves some space.
    It aint much, but its honest work.

--- Problems/Easy/Number_of_1_bits/Java_Damon.java
+++ Problems/Easy/Number_of_1_bits/Java_Damon.java
@@ -1,18 +1,16 @@
 package Easy.Number_of_1_bits;
 
-public class Java_Damon {
-    public class Solution {
-        // you need to treat n as an unsigned value
-        public int hammingWeight(int n) {
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
 
-            int mask = 1, numOfOnes = 0;
+        int mask = 1, numOfOnes = 0;
 
-            for (int bitPos = 0; bitPos < 32; bitPos++) {
-                if ((mask & n) != 0)
-                    numOfOnes++;
-                mask <<= 1;
-            }
-            return numOfOnes;
+        for (int bitPos = 0; bitPos < 32; bitPos++) {
+            if ((mask & n) != 0)
+                numOfOnes++;
+            mask <<= 1;
         }
+        return numOfOnes;
     }
 }
