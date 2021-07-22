ac1f0bb3ede0941fe4713a7e949c0c2bbddc407b
Damon Estrada <damon.estrada@student.nmt.edu>
2021-07-21 20:51:04 -0700

    Solution to problem. It's not much but its honest work.

---
+++ Problems/Medium/Repeated_DNA_Sequences/Java_Damon.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+
+        HashSet<String> seqs = new HashSet<>();
+        HashSet<String> repeats = new HashSet<>();
+        List<String> results = new ArrayList<>();
+
+        int maxSeqLen = 10;
+        String seq;
+
+        // We want to loop until our last check includes the last character in the s string.
+        for (int i = 0; i + maxSeqLen <= s.length(); i++) {
+            seq = s.substring(i, i + maxSeqLen); // get the sub-sequence from now until 10 from now
+            if (seqs.contains(seq))
+                repeats.add(seq); // If we have seen this sequence before.
+            else
+                seqs.add(seq); // add sequence to the map for future checking.
+        }
+
+        return new ArrayList<String>(repeats);
+    }
+}
